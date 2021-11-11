d63afeece62547227cd7d0150f738405a2e09954
Galynin Vlad <shudderyvlados@gmail.com>
2021-11-11 17:12:01 +0300

    fix

--- lab8/i_can_win/src/main/java/page/PastebinHomePage.java
+++ lab8/i_can_win/src/main/java/page/PastebinHomePage.java
@@ -16,7 +16,7 @@ public class PastebinHomePage {
 
     private By expirationSelectLocator = By.xpath("//span[text()='Never']");
 
-    private By expirationChoiceLocator = By.xpath("//li[@id='select2-postform-expiration-result-7o8a-10M']");
+    private By expirationChoiceLocator = By.xpath("/html/body/span[2]/span/span[2]/ul/li[3]");
 
     private WebElement nameInput;
     private By nameInputLocator = By.name("PostForm[name]");
@@ -47,7 +47,7 @@ public class PastebinHomePage {
     public PastebinHomePage selectExpiration() {
         JavascriptExecutor executor = (JavascriptExecutor) driver;
         executor.executeScript("arguments[0].click();", findElementByLocator(expirationSelectLocator));
-        waitVisibilityOfElementLocated(expirationChoiceLocator).click();
+        executor.executeScript("arguments[0].click();", findElementByLocator(expirationChoiceLocator));
         return this;
     }
 
