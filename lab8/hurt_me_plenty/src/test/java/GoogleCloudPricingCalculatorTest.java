bc041536b43c621b05e017ca70b9b3a04bbd31e2
Galynin Vlad <shudderyvlados@gmail.com>
2021-11-11 16:02:39 +0300

    update

--- lab8/hurt_me_plenty/src/test/java/GoogleCloudPricingCalculatorTest.java
+++ lab8/hurt_me_plenty/src/test/java/GoogleCloudPricingCalculatorTest.java
@@ -1,5 +1,3 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -23,11 +21,11 @@ public class GoogleCloudPricingCalculatorTest {
 
     @BeforeMethod(alwaysRun = true)
     public void setupBrowser() {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        WebDriverManager.chromedriver().setup();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
-        driver.manage().window().setSize(new Dimension(1440, 900));
     }
 
     @Test
